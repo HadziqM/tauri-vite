50ab0042d17a481123a706988f119cba4cb2a0de
HadziqM <dimascrazz@gmail.com>
2022-09-30 11:15:26 +0700

    First Commit

---
+++ Kasir/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Test from "./component/test";
+import Page from "./page";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>
+);
